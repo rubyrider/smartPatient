9e0e2cb6dc2610389887244328f214d08c427551
rubyrider <irfan@e-solutionpark.com>
2014-04-02 22:54:37 +0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmartPatient::Application.config.secret_key_base = '51e227a5929d138965be9f607f3dda04641b336e6949f1bf665adb3306d1b603d4d7ef98bc72876d49a2237ce881eb58e800e9d1bc63df129c0760767cf02f88'
